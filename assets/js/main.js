e0ca948ab29d847c23aa38c1e976372e10199bd4
B0kCh01 <nath49ers@gmail.com>
2019-05-25 14:12:40 -0700

    video autoplay fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+$('#myVideo').on('ended', function () {
+  this.load();
+  this.play();
+});
 
 (function($) {
 
